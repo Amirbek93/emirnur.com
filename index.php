3c24cb2c11bd87742ffc5a9ef96d4263fed3ef11
Amirbek Akramov <amirbek2693@gmail.com>
2023-06-10 21:07:04 -0400

    Structure fix by Amirbek

--- index.php
+++ index.php
@@ -9,43 +9,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <link rel="stylesheet" href="css/css.css">
+    <script src="js/js.js"></script>
 </head>
 <body>
+<video id="video-background" src="img/wireframe_-_26097%20(720p).mp4"autoplay loop muted></video>
 <div class="container">
 
-    <table class="table table-border">
-        <tr>
-            <th>Id</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Address</th>
-        </tr>
-        <tr>
-            <td>Id</td>
-            <td>First Name</td>
-            <td>Last Name</td>
-            <td>Email</td>
-            <td>Phone</td>
-            <td>Address</td>
-        </tr>
-    </table>
-
+    <?php
+    include_once "header.php";
+    include_once "Aboutus.php"
+    ?>
 </div>
-<style>
-  .container tr th{
-      background-color: red;
-      color: gold;
-  }
-    .container {
-    width: 100%;
-    margin: 100px auto;
-  }
-    body
-    {
-        background: linear-gradient(to left, #c7ec17, #f3a606, #65da8e);
-    }
-</style>
 </body>
 </html>
